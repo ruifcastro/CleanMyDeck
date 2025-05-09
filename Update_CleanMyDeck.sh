9867b023830cd6c92714bb6fd4de35ded9095e42
Rui Castro <ruifcastro@gmail.com>
2025-05-09 15:46:56 +0100

    Create Update_CleanMyDeck.sh

---
+++ Update_CleanMyDeck.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+REPO_URL="https://github.com/ruifcastro/CleanMyDeck.git"
+TARGET_DIR="$HOME/homebrew/plugins/CleanMyDeck"
+
+if [ ! -d "$TARGET_DIR/.git" ]; then
+  rm -rf "$TARGET_DIR"
+  git clone "$REPO_URL" "$TARGET_DIR"
+else
+  cd "$TARGET_DIR" && git pull
+fi
